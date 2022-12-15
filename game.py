1c18ffa1f664ec04c30d1bb1a8ea488a1d5e4cf9
unknown <jocelynlemer@yahoo.com>
2022-12-14 22:23:31 -0600

    I fixed my classes layout. I had made an error but caught it and fixed it

---
+++ game.py
@@ -0,0 +1,26 @@
+
+class Game:
+    def __init__(self):
+        pass
+
+    def start_game(self):
+        pass
+
+    def play_game(self):
+        pass
+
+    def display_winner(self):
+        pass
+
+    def run_game(self):
+        pass
+
+
+    class Single_Player(Game):
+        def __init__(self) -> None:
+            super().__init__()
+
+
+    class Multiplayer(Game):
+        def __init__(self) -> None:
+            super().__init__()
---
+++ person.py
@@ -0,0 +1,8 @@
+class Person:
+
+    def __init__(self):
+        self.choice_list= ['ROCK', 'PAPER', 'SCISSORS', 'LIZARD', 'SPOCK']
+
+
+    def display_choice(self):
+        pass
